a4b1662826fb4f5e0bcac7ec58c9c9ec8ad63e8b
mashoun <mashoun.me@gmail.com>
2024-06-29 15:55:05 +0300

    fixed title already exist issue

--- js/store.js
+++ js/store.js
@@ -1,13 +1,13 @@
 import Page from './classes/Page.js'
 export default {
-    version: '1.4',
+    version: '1.5',
     devmode:false,
     OAuthUser: false,
     username: '',
     password: '',
     isLogedIn: false,
     geminiToken: '',
-    api: 'https://script.google.com/macros/s/AKfycbyGJq2iVwVJ4wV_sp_eirRD0fJGUs_7bJ_lTWJRRnxBiq0ge7OmHhKKbuKohQfQa3p-/exec',
+    api: 'https://script.google.com/macros/s/AKfycbxKQD9Y5RTA9J52wAVcIeTMv3W36sQypUqhCpEuvpHKlh8jj14rxRGlCyecZjiyqME/exec',
 
     spinner: false,
     services: [],
