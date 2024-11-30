b42d1232778a3829bad69d629a92b6535260d279
mashoun <mashoun.me@gmail.com>
2024-11-30 11:29:58 +0200

    *title already exist api bug fixed

--- js/store.js
+++ js/store.js
@@ -7,7 +7,7 @@ export default {
     password: '',
     isLogedIn: false,
     geminiToken: '',
-    api: 'https://script.google.com/macros/s/AKfycbxKQD9Y5RTA9J52wAVcIeTMv3W36sQypUqhCpEuvpHKlh8jj14rxRGlCyecZjiyqME/exec',
+    api: 'https://script.google.com/macros/s/AKfycbxeFDH2WxafY7VvfPaR9kEm6ZK8b00sFKektX8v0I7poBs3OgaMPR37oju0wkCZC6JG/exec',
 
     spinner: false,
     services: [],
