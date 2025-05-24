44377859b1406bf5ffeaff95be0d3a602f05e786
mashoun <mashoun.me@gmail.com>
2025-05-24 09:26:18 +0300

    Refactor contact section component: streamline data structure and enhance sendMessage method

--- js/components/contact-section/index.js
+++ js/components/contact-section/index.js
@@ -1,24 +1,44 @@
-import utilities from "../../utilities.js"
-import store from '../../store.js'
+import utilities from "../../utilities.js";
+import store from "../../store.js";
 export default {
-    template: await utilities.getPage('/js/components/contact-section/index.html'),
-    data() {
-        return {
-            store,
-            utilities,
-            inputName: '',
-            inputSubject: '',
-            inputMessage: '',
-        }
+  template: await utilities.getPage("/js/components/contact-section/index.html"),
+  data() {
+    return {
+      store,
+      utilities,
+      inputName: "",
+      inputSubject: "",
+      inputMessage: "",
+      api: "https://script.google.com/macros/s/AKfycbwZj7HPNroJve8I6g83ORori0VWZBlq6abRA-9804vshfQxMQEKJwOsTRnORWSVHSbb/exec",
+    };
+  },
+  computed: {
+    // sendMessage() {
+    //     return `mailto:${this.store.contact.email}?subject=${encodeURIComponent(this.inputSubject)}&body=${encodeURIComponent('Name: ' + this.inputName + ' - \n\n' + this.inputMessage)}`
+    // }
+    
+  },
+  methods: {
+    sendMessage() {
+      let payload = {
+        name: this.inputName,
+        subject: this.inputSubject,
+        message: this.inputMessage,
+      };
+      fetch(this.api, {
+        method: "POST",
+        headers: {
+          "Content-Type": "text/plain",
+        },
+        body: JSON.stringify(payload),
+      }).then((res) => {
+        console.log(res);
+        alert('Thanks! Your message has been sent successfully.')
+      });
     },
-    computed: {
-        sendMessage() {
-            return `mailto:${this.store.contact.email}?subject=${encodeURIComponent(this.inputSubject)}&body=${encodeURIComponent('Name: ' + this.inputName + ' - \n\n' + this.inputMessage)}`
-        }
+    sendMessageTo() {
+      //   location.href = this.sendMessage;
+      this.sendMessage();
     },
-    methods: {
-        sendMessageTo() {
-            location.href = this.sendMessage
-        }
-    }
-}
+  },
+};
